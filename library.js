0aecf0de4ea8bb8995ae9d2eeee90feb3520a4db
TedVJ <tedr56@gmail.com>
2014-10-15 20:21:42 +0200

    Add "talks" in regex

--- library.js
+++ library.js
@@ -2,14 +2,14 @@
 	"use strict";
 
 	var ted = {},
-		embed = '<iframe src="https://embed-ssl.ted.com/$1" width="560" height="315" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+		embed = '<iframe src="https://embed-ssl.ted.com/talks/$1" width="560" height="315" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
 
 
 
 https://www.ted.com/talks/glenn_greenwald_why_privacy_matters
 
 	Ted.parse = function(postContent, callback) {
-		postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:ted\.com)\/?(.+)">.+<\/a>/g, embed);
+		postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:ted\.com\/talks)\/?(.+)">.+<\/a>/g, embed);
 		callback(null, postContent);
 	};
 
