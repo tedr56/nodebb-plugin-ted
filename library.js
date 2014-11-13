ba8b4d32b5063f8c9761abf1c49683df3d52eae0
TedVJ <tedr56@gmail.com>
2014-11-13 22:01:51 +0100

    Update to 0.2.0 - NodeBB 0.6.0 compatibility

--- library.js
+++ library.js
@@ -4,13 +4,18 @@
 	var Ted = {},
 		embed = '<iframe src="https://embed-ssl.ted.com/talks/$1.html" width="560" height="315" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
 
-
+    var embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:ted\.com\/talks)\/?(.+)">.+<\/a>/g;
 
 https://www.ted.com/talks/glenn_greenwald_why_privacy_matters
 
-	Ted.parse = function(postContent, callback) {
-		postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:ted\.com\/talks)\/?(.+)">.+<\/a>/g, embed);
-		callback(null, postContent);
+	Ted.parse = function(data, callback) {
+	    if (!data || !data.postData || !data.postData.content) {
+            return callback(null, data);
+        }
+        if (data.postData.content.match(embedUrl)) {
+            data.postData.content = data.postData.content.replace(embedUrl, embed);
+        }
+        callback(null, data);
 	};
 
 	module.exports = Ted;
